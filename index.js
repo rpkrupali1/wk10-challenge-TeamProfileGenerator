48cc59e5cce8219368b32aef9cdaec0ef829f242
krupali pilgulwar <rpkrupali@gmail.com>
2022-02-05 06:18:57 -0600

    Teams array is ready

--- index.js
+++ index.js
@@ -1,3 +1,69 @@
-const ProfileGenerator = require('./lib/ProfileGenerator');
-const profileGenerator = new ProfileGenerator();
-profileGenerator.getTeamMemberInfo();
+const inquirer = require('inquirer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+var teamMembers = [];
+
+const getTeamMemberInfo = () => {        
+    return inquirer.prompt([
+        { 
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Required) ',
+        validate: name => name ? true : false
+        },
+        {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your valid email (Required)',
+        validate: email => email ? true : false
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Please select your role (Required)',
+            choices:  ['Manager', 'Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github user name? (Required)',
+            when: answers => answers.role === 'Engineer',
+            validate: role => role ? true : false
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is your scool name? (Required)',
+            when: answers => answers.role === 'Intern',
+            validate: school => school ? true : false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddTeamMember',
+            message: 'Would you like to add another team member?',
+            default: false
+        }
+    ])
+    .then(teamMemberData => {
+        let employee;
+        if(teamMemberData.role==='Manager')
+            employee = new Manager(teamMemberData.name,teamMemberData.email);
+        else if(teamMemberData.role==='Engineer')
+            employee = new Engineer(teamMemberData.name,teamMemberData.email,teamMemberData.github);
+        else if(teamMemberData.role==='Intern')
+            employee = new Intern(teamMemberData.name,teamMemberData.email,teamMemberData.school);
+        
+        teamMembers.push(employee);
+
+        if(teamMemberData.confirmAddTeamMember)
+            return this.getTeamMemberInfo();               
+        else
+            return teamMembers;
+    });   
+};
+
+
+
+
+
